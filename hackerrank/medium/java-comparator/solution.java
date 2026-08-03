a0b5d0bc326b2782b079972bd094e80e3515ac8a
charithbandi <charithbandi5@gmail.com>
2026-08-03 11:38:24 +0530

    ✅ Java Comparator | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-03

---
+++ hackerrank/medium/java-comparator/solution.java
@@ -0,0 +1,17 @@
+
+
+import java.util.Comparator;
+
+// Write your Checker class here
+class Checker implements Comparator<Player> {
+    @Override
+    public int compare(Player a, Player b) {
+        // If scores are the same, sort alphabetically by name (ascending)
+        if (a.score == b.score) {
+            return a.name.compareTo(b.name);
+        }
+        
+        // Otherwise, sort by score decreasing (highest first)
+        return Integer.compare(b.score, a.score);
+    }
+}
